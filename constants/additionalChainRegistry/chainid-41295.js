7360851de06bca89f1b17ab145b433b4e35bb92e
darlahuang <kakoiiiii@gmail.com>
2025-07-02 12:58:31 +0530

    Create chainid-41295.js

---
+++ constants/additionalChainRegistry/chainid-41295.js
@@ -0,0 +1,25 @@
+{
+  "name": "rootVX Mainnet",
+  "chain": "rootVX",
+  "rpc": [
+    "https://eth.llamarpc.com",
+  ],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "Ether",
+    "symbol": "ETH",
+    "decimals": 18
+  },
+  "features": [{ "name": "EIP7702" }, { "name": "EIP1559" }, { "name": "EIP155" }],
+  "infoURL": "https://www.rootvx.com/",
+  "shortName": "rootVX",
+  "chainId": 41295,
+  "networkId": 42079,
+  "icon": "rootVX",
+  "explorers": [{
+    "name": "rootVXscan",
+    "url": "https://explorer.rootvx.com",
+    "icon": "rootVXscan",
+    "standard": "EIP3091"
+  }]
+}
